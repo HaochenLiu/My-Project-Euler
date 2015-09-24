5fa55b80d3941da0b1556a760b44c7c2f0d31e73
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-24 04:40:26 -0700

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -12,41 +12,41 @@ int max(int a, int b) {
 }
 
 int reverse(int n) {
-	if(n <= 0) return 0;
-	int res = 0;
-	while(n > 0) {
-		res *= 10;
-		res += n % 10;
-		n /= 10;
-	}
+    if(n <= 0) return 0;
+    int res = 0;
+    while(n > 0) {
+        res *= 10;
+        res += n % 10;
+        n /= 10;
+    }
 
-	return res;
+    return res;
 }
 
 bool isPalindrome(int n) {
-	if(n < 0) return false;
-	return (n == reverse(n));
+    if(n < 0) return false;
+    return (n == reverse(n));
 }
 
 int main() {
-	set<int> product;
-	for(int i = 100; i < 1000; i++) {
-		for(int j = i; j < 1000; j++) {
-			product.insert(i * j);
-		}
-		cout<<i<<endl;
-	}
+    set<int> product;
+    for(int i = 100; i < 1000; i++) {
+        for(int j = i; j < 1000; j++) {
+            product.insert(i * j);
+        }
+        cout<<i<<endl;
+    }
 
-	set<int>::reverse_iterator it = product.rbegin();
-	while(it != product.rend()) {
-		cout<<*it<<endl;
-		if(isPalindrome(*it)) {
-			cout<<"res "<<*it<<endl;
-			break;
-		} else {
-			it++;
-		}
-	}
+    set<int>::reverse_iterator it = product.rbegin();
+    while(it != product.rend()) {
+        cout<<*it<<endl;
+        if(isPalindrome(*it)) {
+            cout<<"res "<<*it<<endl;
+            break;
+        } else {
+            it++;
+        }
+    }
 
     int wait = 0;
     cin>>wait;
