ac995cdc3eadfcb34dc9013e42ad083435460985
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-24 04:01:48 -0700

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,8 +1,18 @@
-int res = 0;
-for(int i = 1; i < 1000; i++) {
-    if(i % 3 == 0 || i % 5 == 0) {
-        res += i;
-    }
-}
+#include <iostream>
 
-return res;
+using namespace std;
+
+int main() {
+    int res = 0;
+    for(int i = 1; i < 1000; i++) {
+        if((i % 3 == 0) || (i % 5 == 0)) {
+            res += i;
+        }
+    }
+	cout<<res<<endl;
+
+	int wait = 0;
+	cin>>wait;
+
+	return 0; 
+}
