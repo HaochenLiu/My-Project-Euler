aa2905c4b7be068571e4c6f5d72ea0b59ec3e273
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-24 23:59:19 -0700

    Create 020.cpp

---
+++ 020.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+const int N = 1000; 
+
+struct bignum {
+    int a[N];
+    int point; 
+} num; 
+
+bignum operator*(bignum x, int y) {
+    bignum z;
+    z.point = x.point;
+    memset(z.a, 0, sizeof(z.a)); 
+
+    for(int i = 0; i < N; i++) {
+        z.a[i] = x.a[i] * y;
+    }
+
+    int carry = 0;
+
+    for(int i = 0; i < N; i++) {
+        z.a[i] = z.a[i] + carry;
+        carry = z.a[i] / 10;
+        z.a[i] %= 10;
+    }
+
+    return z;
+} 
+
+bignum operator*(bignum x, bignum y) {
+    bignum z;
+    z.point = x.point + y.point;
+    memset(z.a, 0, sizeof(z.a)); 
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j <= i; j++) {
+            z.a[i] += x.a[j] * y.a[i - j];
+        }
+    }
+
+    int carry = 0;
+
+    for(int i = 0; i < N; i++) {
+        z.a[i] = z.a[i] + carry;
+        carry = z.a[i] / 10;
+        z.a[i] %= 10;
+    }
+
+    return z;
+} 
+                 
+bignum square(bignum x) {
+    return x * x;
+}
+
+/*
+x^y
+*/
+bignum operator&(bignum x,int y) {
+    if(y == 1) return x;
+    if((y & 1) == 0) {
+        return square(x & (y >> 1));
+    } else {
+        return x * square(x & (y >> 1));
+    }
+}
+
+void printNum(bignum num) {
+    for(int i = 0; i < N; i++) {
+        cout<<num.a[i];
+    }
+    cout<<endl;
+    cout<<"num.point "<<num.point<<endl;
+
+    return;
+}
+
+int main() {
+    bignum num;
+    memset(num.a, 0, sizeof(num.a));
+    num.a[0] = 1;
+    for(int i = 2; i <= 100; i++) {
+        num = (num * i);
+    }
+
+    int res = 0;
+    for(int i = 0; i < N; i++) {
+        res += num.a[i];
+    }
+
+    cout<<res<<endl;
+
+    int wait = 0;
+    cin>>wait;
+
+    return 0; 
+}
