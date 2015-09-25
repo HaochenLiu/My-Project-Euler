4d34f4b8f4a6cf3bb72bf72c8ef8096e1e6827fd
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-24 17:09:04 -0700

    Create 007.cpp

---
+++ 007.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int N = 200001;
+    bool* flag = new bool[N];
+    memset(flag, true, sizeof(flag));
+    flag[0] = false;
+    flag[1] = false;
+    for(int i = 2; i <= sqrt((double)N); i++) {
+        for(int j = 2 * i; j < N; j += i) {
+            flag[j] =false;
+        }
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < N; i++) {
+        if(flag[i]) {
+            cnt++;
+            if(cnt == 10001) {
+                cout<<"res = "<<i<<endl;
+                break;
+            }
+        }
+    }
+
+    delete[] flag;
+
+    int wait = 0;
+    cin>>wait;
+
+    return 0; 
+}
