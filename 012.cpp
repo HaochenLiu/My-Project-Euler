dd074de254ac4e66de68cce4a000a7d3c5886953
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-24 19:02:32 -0700

    Create 012.cpp

---
+++ 012.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+#include <unordered_map>
+
+using namespace std;
+
+int getFactorNum(int n) {
+    if(n == 1) return 1;
+    unordered_map<int, int> m;
+    for(int i = 2; i <= n; i++) {
+        if(n % i == 0) {
+            m[i]++;
+            n /= i;
+            i--;
+        }
+    }
+
+    int res = 1;
+    unordered_map<int, int>::iterator it = m.begin();
+    while(it != m.end()) {
+        res *= (it->second + 1);
+        it++;
+    }
+
+    return res;
+}
+
+int tri(int n) {
+    return (n * (n + 1) /2 );
+}
+
+int main() {
+    for(int i = 1; i < 20000; i++) {
+        cout<<i<<" "<<getFactorNum(tri(i))<<endl;
+        if(getFactorNum(tri(i)) > 500) {
+            cout<<i<<" "<<tri(i)<<" "<<getFactorNum(tri(i))<<endl;
+            break;
+        }
+    }
+
+    int wait = 0;
+    cin>>wait;
+
+    return 0; 
+}
