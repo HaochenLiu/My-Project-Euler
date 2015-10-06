231caa642689d9beb57ceb21664cbfb09b14e192
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-06 07:51:20 -0700

    Create 073.cpp

---
+++ 073.cpp
@@ -0,0 +1,44 @@
+/*
+http://www.mathblog.dk/project-euler-73-sorted-reduced-proper-fractions/
+*/
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <functional>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    if(a == b) return a;
+    if(a < b) return gcd(b, a);
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a = 3;
+    int b = 2;
+    int limit = 12000;
+    int result = 0;
+ 
+    for(int d = 5; d <= limit; d++) {
+        for(int n = d / a + 1; n < (d - 1) / b + 1; n++) {
+            if(gcd(n, d) == 1) {
+                result++;
+            }
+        }
+    }
+    cout<<result<<endl;
+
+    int wait = 0;
+    cin>>wait;
+
+    return 0; 
+}
