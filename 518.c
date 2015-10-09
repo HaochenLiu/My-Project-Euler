b897da38e134a5a7c8b84d6c369628a218eac0b3
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-09 15:32:13 -0700

    Create 518.c

---
+++ 518.c
@@ -0,0 +1,72 @@
+#define true 1
+#define false 0
+
+char *prime;
+const int N = 100000000;
+
+char isGeoSeq(long long a, long long b, long long c) {
+    return (a * c == b * b);
+}
+
+int max(int a, int b) {
+    if(a > b) return a;
+    return b;
+}
+
+int sqrt(int n) {
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+    int i = 1;
+    for(i = 1; i <= n; i++) {
+	if(i * i == n) return i;
+	if(i * i > n) return i - 1;
+    }
+    return -1;
+}
+
+int main() {
+    prime = malloc(N * sizeof(int));
+    memset(prime, true, sizeof(char) * N);
+    prime[0] = false;
+    prime[1] = false;
+    int sr = sqrt(N);
+    for(int i = 2; i <= sqrt(N); i++) {
+        for(int j = 2 * i; j < N; j += i) {
+            prime[j] = false;
+        }
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < N; i++) {
+        if(prime[i]) {
+            cnt++;
+        }
+    }
+    printf("prime cnt = %d\n", cnt);
+
+    long long* p = malloc(sizeof(long long) * cnt);
+    int j = 0;
+    for(int i = 0; i < N; i++) {
+        if(prime[i]) {
+            p[j++] = i;
+        }
+    }
+    
+    long long sum = 0;
+    for(int a = 0; a < cnt; a++) {
+        if(a % 10000 == 0) printf("calulated %d\n", a);
+        for(int b = a + 1; b < cnt; b++) {
+            long long t = (p[b] + 1) * (p[b] + 1) / (p[a] + 1) - 1;
+            if(t >= N) break;
+            if((p[b] + 1) * (p[b] + 1) % (p[a] + 1) != 0) continue;
+            if(prime[t]) {
+                sum += p[a];
+                sum += p[b];
+                sum += t;
+            }
+        }
+    }
+    printf("sum = %d\n", sum);
+
+    return 0; 
+}
