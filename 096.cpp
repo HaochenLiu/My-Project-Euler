151bd04d58072c2fb26551a83d462e5753e29fa7
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-10 15:21:41 -0700

    Update 096.cpp

--- 096.cpp
+++ 096.cpp
@@ -83,9 +83,9 @@ int main() {
             cout<<endl;
         }
         cout<<endl;
-        res += 100 * (board[0][6] - '0');
-        res += 10 * (board[0][7] - '0');
-        res += (board[0][8] - '0');
+        res += 100 * (board[0][0] - '0');
+        res += 10 * (board[0][1] - '0');
+        res += (board[0][2] - '0');
     }
 
     cout<<"Result = "<<res<<endl;
