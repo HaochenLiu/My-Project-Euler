518222ada47d7c16a1493f55b102e533260b2c8a
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-11 00:35:03 -0700

    Create 158.java

---
+++ 158.java
@@ -0,0 +1,24 @@
+    
+
+	public static void main(String[] args) throws Exception {
+		long[] base = new long[27];
+		base[2] = 1;
+		for (int i = 3; i <= 26; ++i) {
+			base[i] = (1 << (i - 1)) - 1 + base[i - 1];
+		}
+		long ans = 0;
+		for (int i = 2; i <= 26; ++i) {
+			ans = Math.max(ans, comb(26, i) * base[i]);
+		}
+		System.out.println(ans);
+	}
+
+	static long comb(int n, int m) {
+		long ans = 1;
+		for (int i = 0; i < m; ++i) {
+			ans *= (n - i);
+			ans /= (i + 1);
+		}
+		return ans;
+	}
+}
