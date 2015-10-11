9a988f5b4bb30f683e503068513158c03823091c
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-10 17:36:04 -0700

    Update 129.cs

--- 129.cs
+++ 129.cs
@@ -1,71 +1,43 @@
 using System;
 using System.Diagnostics;
 
-namespace euler
-{
-    class Problem128
-    {
-        public static void Main(string[] args)
-        {
-            new Problem128().Bruteforce();
+namespace euler {
+    class Problem129 {
+        public static void Main(string[] args) {
+            new Problem129().Bruteforce();
         }
 
-        public void Bruteforce()
-        {
+        public void Bruteforce() {
             Stopwatch clock = Stopwatch.StartNew();
+            
+            int limit = 1000001;
+            int n = limit;
 
-            int count = 1;
-            int limit = 2000;
-            long n = 0;
-            long number = 0;
-
-            while (count < limit)
-            {
-                n++;
-                if (IsPrime(6 * n - 1) && IsPrime(6 * n + 1) && IsPrime(12 * n + 5))
-                {
-                    count++;
-                    number = (3 * n * n - 3 * n + 2);
-                    if (count >= limit) break;
-
-                }
-                if (IsPrime(6 * n + 5) && IsPrime(6 * n - 1) && IsPrime(12 * n - 7) && n != 1)
-                {
-                    count++;
-                    number = (3 * n * n + 3 * n + 1);
-                }
+            while (A(n) < limit) {                
+                n += 2;
             }
-
+                  
             clock.Stop();
-            Console.WriteLine("The {0}th number is {1}", limit, number);
+            Console.WriteLine("The least value of n A(N) which first exceeds one-million {0}", n);
             Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
         }
 
-        public static bool IsPrime(long n)
-        {
-            if (n < 2)
-                return false;
-            if (n < 4)
-                return true;
-            if (n % 2 == 0)
-                return false;
-            if (n < 9)
-                return true;
-            if (n % 3 == 0)
-                return false;
-            if (n < 25)
-                return true;
+        private int A(int n) {
+            if (GCD(n, 10) != 1) return 0;
 
-            int s = (int)Math.Sqrt(n);
-            for (int i = 5; i <= s; i += 6)
-            {
-                if (n % i == 0)
-                    return false;
-                if (n % (i + 2) == 0)
-                    return false;
+            int x = 1;
+            int k = 1;
+
+            while (x != 0) {
+                x = (x * 10 + 1) % n;
+                k++;
             }
 
-            return true;
+            return k;
+        }
+
+        private int GCD(int a, int b) {
+            return b == 0 ? a : GCD(b, a % b);
         }
     }
 }
