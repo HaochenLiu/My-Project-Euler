449b2dab3e130af96ed047eef268433e967d93b6
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-17 17:38:43 -0700

    Update 066.cpp

--- 066.cpp
+++ 066.cpp
@@ -791,7 +791,6 @@ bignum fac(int n) {
 }
 
 long long gcd(long long a, long long b) {
-    if(a == b) return a;
     if(a < b) return gcd(b, a);
     if(b == 0) return a;
     return gcd(b, a % b);
