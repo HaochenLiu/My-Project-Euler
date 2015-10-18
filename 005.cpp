5b055ddbc4cf61ad51bbc053f9f7267eaf718a72
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-17 17:29:33 -0700

    Update 005.cpp

--- 005.cpp
+++ 005.cpp
@@ -1,10 +1,8 @@
-#include <set>
 #include <iostream>
 
 using namespace std;
 
 long long gcd(long long a, long long b) {
-    if(a == b) return a;
     if(a < b) return gcd(b, a);
     if(b == 0) return a;
     return gcd(b, a % b);
